1e294ae200d4e7a4ca78114be86626a0b4981054
whalemo(莫金雨) <whalemo@tencent.com>
2018-03-21 23:03:05 +0800

    Add serial read and control flags.

--- src/Serial.cpp
+++ src/Serial.cpp
@@ -1,5 +1,6 @@
 #include "Serial.h"
 #include "Joystick.h"
+#include "FixedRingBuffer.h"
 
 enum {
 	INIT = 0,
@@ -7,7 +8,13 @@ enum {
 	W_NUMBER
 };
 
-Serial_Control_Type control_map_buffer[buffer_size];
+FixedRingBuffer<Serial_Control_Type> control_map_buffer;
+Serial_Control_Type& write_control = control_map_buffer.tail();
+Serial_Control_Type& read_control = control_map_buffer.head();
+bool hard_reset = false;
+int delay_count = 0;
+bool wait_for_report = false;
+unsigned char sync_report_count;
 
 // Only 1 byte for tx, only empty cache reported.
 unsigned char tx_byte = 0;
@@ -23,6 +30,7 @@ ISR(USART1_RX_vect) {
 		// It's a control byte.
 		// Write control byte to buffer.
 		// Reset data_count everytime when received control byte.
+		write_control.control = rx_byte;
 		data_count = 0;
 		switch (rx_byte & 0x70) {
 		case 0x00:
@@ -40,25 +48,40 @@ ISR(USART1_RX_vect) {
 		case 0x60:
 			// Update All	1	1	1	0
 			data_expect = 7;
+			break;
+		case 0xF0:
+			// Reset All	1	1	1	1
+			hard_reset = rx_byte == 0xFF;
+			data_expect = -1;
+			break;
 		default:
 			// HAT	1	0	0	1
 			// Reset	1	1	0	0
-			// Reset All	1	1	1	1
 			data_expect = 0;
-			break;
 			break;
 		}
 	} else {
 		// It's a data byte.
 		if (data_count < data_expect) {
 			// Fill this data buffer, add count
+			write_control.data[data_count] = rx_byte;
 			++data_count;
 		}
 	}
 
 	if(data_count == data_expect) {
 		// Full filled, move next.
+		write_control = control_map_buffer.write();
+		Report_Count(control_map_buffer.free());
 		data_expect = -1;
+	}
+}
+
+void Report_Count(unsigned char count) {
+	if(UCSR1A & (1 << UDRE1)) {
+		UDR1 = count;
+	} else {
+		tx_byte = count;
 	}
 }
 
@@ -84,14 +107,33 @@ void Serial_Init() {
 }
 
 void Serial_Task() {
-	// Do timing & modify joystick data here.
-	// Parse CMDs here.
-}
+	// if hard_reset flag is set, reset all state at once.
+	if (hard_reset) {
+		control_map_buffer.clear();
+		Report_Count(control_map_buffer.free());
+		hard_reset = false;
+		wait_for_report = false;
+		delay_count = 0;
+		return;
+	}
 
-void Report_Count(unsigned char count) {
-	if(UCSR1A & (1 << UDRE1)) {
-		UDR1 = count;
-	} else {
-		tx_byte = count;
+	if (wait_for_report) {
+		if (sync_report_count != Joystick_Report_Count) return;
+		wait_for_report = false;
+	}
+
+	if (delay_count > 0) {
+		// wait for timer clear this flag.
+		return;
+	}
+	// Do timing & modify joystick data here.
+	if(control_map_buffer.isAvailable()) {
+		read_control = control_map_buffer.read();
+		switch (read_control.control) {
+		default:
+			break;
+		}
 	}
 }
+
+
